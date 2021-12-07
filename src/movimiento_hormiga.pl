3ec98bed9ac1423036d2bfa28232a4f6126f0206
Carlos <carlozalejandro98@gmail.com>
2021-12-07 17:48:18 -0500

    movimiento hormiga al berro

---
+++ src/movimiento_hormiga.pl
@@ -0,0 +1,69 @@
+:- import(utiles_movimiento).
+:- import(utiles).
+:- import(tablero).
+
+
+:- [utiles_movimiento, tablero, utiles].
+
+:-dynamic caminoAnterior/1.
+
+guardarCamino(Camino):-
+    retractall(caminoAnterior(_)),
+    assert(caminoAnterior(Camino)).
+
+moverHormiga(Hormiga, Ficha, Jugador, Cara):-
+    turno(JugadorActual),
+    arista(Jugador,Ficha,Cara,X,Y),
+    aristasDeLaPieza(JugadorActual, Hormiga, L),
+    assert(camino([L])),
+    eliminaAristas(Hormiga, L),
+    eliminaConexiones(Hormiga),
+    moverPorTodasLasCaras(Hormiga, X, Y).
+
+moverPorCara(Hormiga, Direccion, X, Y):-
+    turno(Jugador1),
+    camino([Casilla|R]),
+    casillaLibreNoCerrada(Casilla, Direccion, CasillaDestino),
+    not(pertenece(CasillaDestino, [Casilla|R])),
+    not(casillaSolitaria(CasillaDestino)),
+    retractall(camino(_)),
+    assert(camino([CasillaDestino,Casilla|R])),
+    ((pertenece(X,Y,CasillaDestino),
+    caminoValido(Hormiga, [Casilla|R]));
+    moverPorTodasLasCaras(Hormiga, X, Y)),
+    !.
+
+moverPorTodasLasCaras(Hormiga, X, Y):-
+    (camino(Camino),
+        guardarCamino(Camino),
+    moverPorCara(Hormiga, 1, X, Y));
+
+    (
+    retractall(camino(_)),
+    caminoAnterior(Camino),
+    assert(camino(Camino)),
+    moverPorCara(Hormiga, 2, X, Y)
+    );
+
+    (retractall(camino(_)),
+    caminoAnterior(Camino),
+    assert(camino(Camino)),
+    moverPorCara(Hormiga, 3, X, Y)
+    );
+
+    (retractall(camino(_)),
+    caminoAnterior(Camino),
+    assert(camino(Camino)),
+    moverPorCara(Hormiga, 4, X, Y)
+    );
+
+    (retractall(camino(_)),
+    caminoAnterior(Camino),
+    assert(camino(Camino)),
+    moverPorCara(Hormiga, 5, X, Y)
+    );
+
+    (retractall(camino(_)),
+    caminoAnterior(Camino),
+    assert(camino(Camino)),
+    moverPorCara(Hormiga, 6, X, Y)).
---
+++ src/utiles_movimiento.pl
@@ -0,0 +1,96 @@
+:- import(tablero).
+:- import(utiles).
+
+:- [tablero, utiles].
+
+
+
+casillaLibreNoCerrada(L, Direccion, CasillaDestino):-
+    write_ln(L),
+    write_ln(Direccion),
+    coordenadasDeArista(L,Direccion,X,Y),
+    coordenadaCaras(Direccion,X,Y,Casilla),
+    
+    incrementoCircular6(Direccion,Adyacente1),
+    coordenadasDeArista(L,Adyacente1,X1,Y1),
+    decrementoCircular6(Direccion,Adyacente2),
+    coordenadasDeArista(L,Adyacente2,X2,Y2),
+
+    coordenadaCaras(Adyacente1,X1,Y1,Casilla1),
+    coordenadaCaras(Adyacente2,X2,Y2,Casilla2),
+    !,
+    (casillaVacia(Casilla1) ; casillaVacia(Casilla2)),
+    casillaVacia(Casilla),
+    CasillaDestino = Casilla.
+
+casillaLibreNoCerrada(Pieza,Jugador,Direccion,CasillaDestino):-
+    casillaVacia(Pieza, Jugador, Direccion),
+    incrementoCircular6(Direccion, Adyacente1),
+    decrementoCircular6(Direccion, Adyacente2),
+    (casillaVacia(Pieza,Jugador,Adyacente1) ; casillaVacia(Pieza,Jugador,Adyacente2)),
+    
+    arista(Jugador,Pieza,Direccion,A,B),
+    coordenadaCaras(Direccion,A,B,CasillaDestino).
+    
+
+casillaVacia([X1,Y1,X2,Y2|R]):-
+    not((arista(J,P,A,X1,Y1), arista(J,P,B,X2,Y2))).
+casillaVacia(Pieza,Jugador,Direccion):-
+    arista(Jugador,Pieza,Direccion,X,Y), 
+    coordenadaCaras(Direccion,X,Y,[X1,Y1,X2,Y2|R]), % dos caras de la casilla q esta en la direccion
+    not((arista(J,P,A,X1,Y1), arista(J,P,B,X2,Y2))). % que las 2 aristas no pertenezcan a una misma ficha
+
+restablecerPapeleras:-
+    findall([Jugador,Pieza,Cara,X,Y], papeleraAristas(Jugador,Pieza,Cara,X,Y), A),
+    reestableceAristas(A),
+    findall([Jugador1,Pieza1,Cara1,Jugador2,Pieza2,Cara2], papeleraConexiones(Jugador1,Pieza1,Cara1,Jugador2,Pieza2,Cara2), B),
+    reestableceConexiones(B),
+    vaciarPapeleras.
+    
+
+reestableceAristas([]).
+reestableceAristas([[Jugador,Pieza,Cara,X,Y]|R]):-
+    assert(arista(Jugador,Pieza,Cara,X,Y)),
+    reestableceAristas(R).
+
+reestableceConexiones([]).
+reestableceConexiones([[Jugador1,Pieza1,Cara1,Jugador2,Pieza2,Cara2]|R]):-
+    assert(conexion(Jugador1,Pieza1,Cara1,Jugador2,Pieza2,Cara2)),
+    reestableceConexiones(R).
+    
+vaciarPapeleras:-
+    retractall(papeleraConexiones(Jugador1,Pieza1,Cara1,Jugador2,Pieza2,Cara2)),
+    retractall(papeleraAristas(_,_,_,_,_)).
+
+
+
+coordenadasDeArista([X1,Y1,X2,Y2,X3,Y3,X4,Y4,X5,Y5,X6,Y6],Cara,X,Y):-
+    Cara is 1,
+    X is X1,
+    Y is Y1,
+    !.
+coordenadasDeArista([X1,Y1,X2,Y2,X3,Y3,X4,Y4,X5,Y5,X6,Y6],Cara,X,Y):-
+    Cara is 2,
+    X is X2,
+    Y is Y2,
+    !.
+coordenadasDeArista([X1,Y1,X2,Y2,X3,Y3,X4,Y4,X5,Y5,X6,Y6],Cara,X,Y):-
+    Cara is 3,
+    X is X3,
+    Y is Y3,
+    !.
+coordenadasDeArista([X1,Y1,X2,Y2,X3,Y3,X4,Y4,X5,Y5,X6,Y6],Cara,X,Y):-
+    Cara is 4,
+    X is X4,
+    Y is Y4,
+    !.
+coordenadasDeArista([X1,Y1,X2,Y2,X3,Y3,X4,Y4,X5,Y5,X6,Y6],Cara,X,Y):-
+    Cara is 5,
+    X is X5,
+    Y is Y5,
+    !.
+coordenadasDeArista([X1,Y1,X2,Y2,X3,Y3,X4,Y4,X5,Y5,X6,Y6],Cara,X,Y):-
+    Cara is 6,
+    X is X6,
+    Y is Y6,
+    !.
