c924d48780e61f122306f684f240839798e080f3
Carlos <carlozalejandro98@gmail.com>
2021-12-09 18:40:23 -0500

    aranha testeada, casi seguro esta terminada

--- src/movimiento_aranha.pl
+++ src/movimiento_aranha.pl
@@ -66,8 +66,7 @@ moverComoAbeja(MiFicha,Ficha,Jugador,Cara):-
     aristasDeLaPieza(Jugador1, MiFicha, R2),
     eliminaAristasPermanente(MiFicha,R2),
     restablecerPapeleras,
-    creaConexiones(MiFicha, A),
-    creaAristas(MiFicha, A),
+    creaConexionesSiNoExisten(Jugador1, MiFicha, A),
     !,
     fail)).
 
@@ -81,23 +80,6 @@ moverAranhaRecursivo(Aranha, Ficha, Jugador, Cara, 0):-
     conexion(JugadorActual,Aranha,_,Jugador,Ficha,Cara),
     writeln("   Dio Tru").
 
-guardarAristasEnPapelera(Jugador, Pieza, [X1,Y1,X2,Y2,X3,Y3,X4,Y4,X5,Y5,X6,Y6]):-
-    assert(papeleraAristas(Jugador, Pieza, 1, X1, Y1)),
-    assert(papeleraAristas(Jugador, Pieza, 2, X2, Y2)),
-    assert(papeleraAristas(Jugador, Pieza, 3, X3, Y3)),
-    assert(papeleraAristas(Jugador, Pieza, 4, X4, Y4)),
-    assert(papeleraAristas(Jugador, Pieza, 5, X5, Y5)),
-    assert(papeleraAristas(Jugador, Pieza, 6, X6, Y6)).
-
-guardarConexionesEnPapelera(Pieza1, Jugador1, []).
-guardarConexionesEnPapelera(Pieza1, Jugador1, [[Cara1,Jugador2,Pieza2,Cara2]|R]):-
-    assert(papeleraConexiones(Jugador1,Pieza1,Cara1,Jugador2,Pieza2,Cara2)),
-    assert(papeleraConexiones(Jugador2,Pieza2,Cara2,Jugador1,Pieza1,Cara1)),
-    guardarConexionesEnPapelera(Pieza1, Jugador1, R).
-
-guardarConexionesEnPapelera(Jugador1, Pieza1):-
-    findall([Cara1,Jugador2,Pieza2,Cara2], conexion(Jugador1,Pieza1,Cara1,Jugador2,Pieza2,Cara2), X),
-    guardarConexionesEnPapelera(Pieza1, Jugador1, X).
 
 moverAranhaRecursivo(Aranha, Ficha, Jugador, Cara, PasosRestantes):-
     turno(JugadorActual),
@@ -125,7 +107,10 @@ posiblePaso(Aranha,Jugador,Ficha,Cara):-
     turno(JugadorActual),
     conexion(JugadorActual,Aranha,Cara1,Jugador,Ficha,Cara2),
     caraAdyacente(Cara2,Cara),
-    write("Estoy en "), write([Jugador, Ficha]), write(" en la cara"), writeln(Cara2).
+    write("Estoy en "), write([Jugador, Ficha]), write(" en la cara"), writeln(Cara2),
+    arista(Jugador, Ficha, Cara2, X, Y),
+    coordenadaCaras(Cara2, X, Y, Aristas).
+
     % findall([J1, P1, C1, J2, P2, C2], conexion(J1, P1, C1, J2, P2, C2), R),
     % writeln(R).
 
@@ -133,17 +118,3 @@ caraAdyacente(Cara, Retorno):-
     incrementoCircular6(Cara,Retorno).
 caraAdyacente(Cara,Retorno):-
     decrementoCircular6(Cara,Retorno).
-
-
-restablecerEstado(Aranha, Aristas):-
-    turno(JugadorActual),
-    writeln("   no movio ni ostias"),
-    write("         Viejas aristas: "), writeln(Aristas),
-    writeln([JugadorActual, Aranha]),
-    aristasDeLaPieza(JugadorActual,Aranha,A),
-    write("         Aristas actuales: "),writeln(A),
-    eliminaAristasPermanente(Aranha,A),
-    eliminaConexionesPermanente(Aranha),
-    creaConexiones(Aranha,Aristas),
-    creaAristas(Aranha,Aristas),!,
-    fail.
